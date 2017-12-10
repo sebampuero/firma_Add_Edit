abacc4f0cc7d61af14a345ba47f5dc7aaae856e4
sebastian <sebatorin@hotmail.com>
2017-12-10 17:31:39 +0100

    Typeahead autocomplete function now working

---
+++ static/js/typeahead_utils.js
@@ -0,0 +1,29 @@
+var firm_names = new Bloodhound({
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    remote: {
+        url: '/firms?query=%QUERY',
+        wildcard: '%QUERY'
+    }
+});
+//Select the Bloodhound object and display the data
+$('input#typeahead').typeahead(null,{
+    name: 'name',
+    display: 'name',
+    source: firm_names
+});
+
+var titles = new Bloodhound({
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('titel'),
+    remote: {
+        url: '/titles?query=%QUERY',
+        wildcard: '%QUERY'
+    }
+});
+//Select the Bloodhound object and display the data
+$('input.typeahead_title').typeahead(null,{
+    name: 'titel',
+    display: 'titel',
+    source: titles
+});
