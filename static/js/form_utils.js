7b55a92bba9e4976cffde3ff1d63f8df576bbecb
sebastian <sebatorin@hotmail.com>
2017-12-12 01:13:44 +0100

    Fixed regex for plz

--- static/js/form_utils.js
+++ static/js/form_utils.js
@@ -1,3 +1,22 @@
+$.validator.addMethod("PLZ_Validator", function(value, element) {
+    var land = $('select#land_select').val();
+    var valid = false;
+    switch(land){
+        case 'DE':
+            if(/(^\d{5}$)/i.test(value)){
+                valid = true;
+            }break;
+        case 'CH':
+            if(/(^\d{4}$)/i.test(value)){
+                valid = true;
+            }break;
+        case 'AT':
+            if(/(^\d{4}$)|(^\d{4}-\d{4}$)/i.test(value)){
+                valid = true;
+            }break;
+    }
+    return this.optional(element) || valid;
+});
 var ans_form_validation_rules = {
     rules: {
        name: 'required',
@@ -31,7 +50,10 @@ var firm_form_validation_rules = {
     rules: {
         name: 'required',
         strasse_hnr: 'required',
-        plz: 'required',
+        plz: {
+            required: true,
+            PLZ_Validator: true
+        },
         ort: 'required',
         website: {
             required: true,
@@ -47,7 +69,10 @@ var firm_form_validation_rules = {
     messages: {
         name: 'Name muss angegeben werden',
         strasse_hnr: 'Eine Adresse muss angegeben werden',
-        plz: 'Eine Postleitzahl muss angegeben werden',
+        plz: {
+            required: 'Eine Postleitzahl muss angegeben werden',
+            PLZ_Validator: 'Ungültige Postleitzahl für das gewählte Land'
+        },
         ort: 'Ein Ort muss angegeben werden',
         website: {
             required: 'Eine Website muss angegeben werden',
