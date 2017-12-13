dd6e6619275d369fe409520d4529dd59a1eeb9a8
sebastian <sebatorin@hotmail.com>
2017-12-14 00:29:43 +0100

    nothing

--- Service/xml_parser.py
+++ Service/xml_parser.py
@@ -34,36 +34,33 @@ def delete_firm(firma_name):
 
 # Insert a new firm into the XML file
 def insert_firm(firma):
-    try:
-        root, tree = get_root()
-        firma_attrib = {'name': firma.name, 'letztes_speichern_datum': firma.letztes_speichern_datum}
-        firma_element = ET.SubElement(root, 'firma', firma_attrib)
-        ET.SubElement(firma_element, 'branche').text = firma.branche
-        ET.SubElement(firma_element, 'strasse_hnr').text = firma.strasse_hnr
-        if firma.adresszusatz != "":
-            ET.SubElement(firma_element, 'adresszusatz').text = firma.adresszusatz
-        ET.SubElement(firma_element, 'plz').text = firma.plz
-        ET.SubElement(firma_element, 'ort').text = firma.ort
-        ET.SubElement(firma_element, 'land').text = firma.land
-        ET.SubElement(firma_element, 'website').text = firma.website
-        for ans in firma.ansprechpartner_liste:
-            ans_attrib = {'name': ans.name}
-            ansprechpartner_element = ET.SubElement(firma_element, 'ansprechpartner', ans_attrib)
-            ET.SubElement(ansprechpartner_element, 'anrede').text = ans.anrede
-            if ans.titel != "":
-                ET.SubElement(ansprechpartner_element, 'titel').text = ans.titel
-            if ans.funktion != "":
-                ET.SubElement(ansprechpartner_element, 'funktion').text = ans.funktion
-            for telefon in ans.telefon:
-                ET.SubElement(ansprechpartner_element, 'telefon').text = telefon
-            if ans.fax != "":
-                ET.SubElement(ansprechpartner_element, 'fax').text = ans.fax
-            ET.SubElement(ansprechpartner_element, 'email').text = ans.email
-        ET.SubElement(firma_element, 'erfassungsdatum').text = firma.erfassungsdatum
-        indent(root)
-        tree.write(filename, encoding="utf-8", xml_declaration=True)
-    except Exception:
-        return None
+    root, tree = get_root()
+    firma_attrib = {'name': firma.name, 'letztes_speichern_datum': firma.letztes_speichern_datum}
+    firma_element = ET.SubElement(root, 'firma', firma_attrib)
+    ET.SubElement(firma_element, 'branche').text = firma.branche
+    ET.SubElement(firma_element, 'strasse_hnr').text = firma.strasse_hnr
+    if firma.adresszusatz != "":
+        ET.SubElement(firma_element, 'adresszusatz').text = firma.adresszusatz
+    ET.SubElement(firma_element, 'plz').text = firma.plz
+    ET.SubElement(firma_element, 'ort').text = firma.ort
+    ET.SubElement(firma_element, 'land').text = firma.land
+    ET.SubElement(firma_element, 'website').text = firma.website
+    for ans in firma.ansprechpartner_liste:
+        ans_attrib = {'name': ans.name}
+        ansprechpartner_element = ET.SubElement(firma_element, 'ansprechpartner', ans_attrib)
+        ET.SubElement(ansprechpartner_element, 'anrede').text = ans.anrede
+        if ans.titel != "":
+            ET.SubElement(ansprechpartner_element, 'titel').text = ans.titel
+        if ans.funktion != "":
+            ET.SubElement(ansprechpartner_element, 'funktion').text = ans.funktion
+        for telefon in ans.telefon:
+            ET.SubElement(ansprechpartner_element, 'telefon').text = telefon
+        if ans.fax != "":
+            ET.SubElement(ansprechpartner_element, 'fax').text = ans.fax
+        ET.SubElement(ansprechpartner_element, 'email').text = ans.email
+    ET.SubElement(firma_element, 'erfassungsdatum').text = firma.erfassungsdatum
+    indent(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
 
 
 # Get the root of the XML File
