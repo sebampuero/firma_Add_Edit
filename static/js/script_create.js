418e97974bffefd2e720e674751d67fbfb8b357e
sebastian <sebatorin@hotmail.com>
2017-12-14 15:45:04 +0100

    JS Code refactor

---
+++ static/js/script_create.js
@@ -0,0 +1,48 @@
+$(function(){
+        //listeners for form checkbox activators
+        $('input:checkbox#ansprechpartner1_checkbox').change(enableOrDisableAnsprechpartnerForm);
+        $('input:checkbox#ansprechpartner2_checkbox').change(enableOrDisableAnsprechpartnerForm);
+
+        $('button#submit_create').on('click',function(){
+            var result = parseFirmObject(); // result contains the firm obj and validation flag
+            $('#loading_spinner').show();
+            if(result.flag){
+                $.ajax({
+                    type: "POST",
+                    url: "/firm/create",
+                    data: JSON.stringify(result.firm_obj),
+                    contentType: 'application/json',
+                    dataType: 'json'
+                }).always(function(data){
+                    if(data.status == 200)
+                        window.location.replace("/?create="+result.firm_obj.name);
+                    else if(data.status == 500)
+                        $('#server_error_modal').modal('show');
+                });
+            }else{
+                $('#invalid_input_modal').modal('show');
+                $('#loading_spinner').hide();
+            }
+        });
+
+        //listener for firm name input to check if the given firm already exists
+        $('input#typeahead').change(function(){
+           var input_text = $(this).val();
+           if(input_text != ""){
+                $.get( "/firms?query="+input_text, function( firm ) {
+                    if(firm.length==1){
+                        if(input_text == firm[0].name){
+                            $('button#submit_create').prop('disabled', true);
+                            $('label#firm_name_error_label').show();
+                        }
+                    }else{
+                        $('button#submit_create').prop('disabled', false);
+                        $('label#firm_name_error_label').hide();
+                    }
+                });
+           }else{
+                $('button#submit_create').prop('disabled', false);
+                $('label#firm_name_error_label').hide();
+           }
+        });
+    });
